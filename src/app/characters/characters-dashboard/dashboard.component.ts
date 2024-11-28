94c8ab7d1d4270f367a906020f00b087329461cd
Gvaldz <233415@ids.upchiapas.edu.mx>
2024-11-28 13:58:41 -0600

    add: se hacen las cards de characters

--- src/app/characters/characters-dashboard/dashboard.component.ts
+++ src/app/characters/characters-dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Characters } from '../characters';
+import { CharactersService } from '../characters.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  characters: Characters[] = []; 
 
+  constructor(private charactersService: CharactersService) {}
+
+  ngOnInit(): void {
+    this.getCharacters(); 
+  }
+
+  getCharacters(): void {
+    this.charactersService.getCharacters().subscribe((response: any) => {
+      this.characters = response.items; 
+    });
+  }
+  
+  
 }
--- src/app/characters/characters.ts
+++ src/app/characters/characters.ts
@@ -1,4 +1,5 @@
 export interface Characters {
+    [x: string]: any;
     name: string,
     ki: number,
     race: string,
