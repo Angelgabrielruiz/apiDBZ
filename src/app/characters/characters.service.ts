a687fdf6f6db28e14e618ae5ded322a3e8891c5d
Angelgabrielruiz <aa09angel@gmail.com>
2024-11-28 13:19:59 -0600

    servicio de consumo de api

--- src/app/characters/characters.service.ts
+++ src/app/characters/characters.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Characters } from './characters';
+import { Planets } from './planets';
+import { Transformations } from './transformations';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharactersService {
+  private charactersUrl = 'https://dragonball-api.com/api/characters';
+  private planetsUrl = 'https://dragonball-api.com/api/planets';
+  private transformationsUrl = 'https://dragonball-api.com/api/transformations';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  getCharacters(): Observable<Characters[]> {
+    return this.http.get<Characters[]>(this.charactersUrl);
+  }
+
+  
+  getCharacterById(id: string): Observable<Characters> {
+    return this.http.get<Characters>(`${this.charactersUrl}/${id}`);
+  }
+
+  
+  getPlanets(): Observable<Planets[]> {
+    return this.http.get<Planets[]>(this.planetsUrl);
+  }
+
+  
+  getPlanetById(id: string): Observable<Planets> {
+    return this.http.get<Planets>(`${this.planetsUrl}/${id}`);
+  }
+
+  
+  getTransformations(): Observable<Transformations[]> {
+    return this.http.get<Transformations[]>(this.transformationsUrl);
+  }
+
+  
+  getTransformationById(id: string): Observable<Transformations> {
+    return this.http.get<Transformations>(`${this.transformationsUrl}/${id}`);
+  }
 }
