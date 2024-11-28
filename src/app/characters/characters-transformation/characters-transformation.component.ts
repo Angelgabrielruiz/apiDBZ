c467bc9eab6cd49a5d3902613d4b234ba62369ce
Gvaldz <233415@ids.upchiapas.edu.mx>
2024-11-28 12:58:20 -0600

    add:se agrega el componente chararacters transformations

---
+++ src/app/characters/characters-transformation/characters-transformation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters-transformation',
+  templateUrl: './characters-transformation.component.html',
+  styleUrl: './characters-transformation.component.css'
+})
+export class CharactersTransformationComponent {
+
+}
--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
+import { CharactersTransformationComponent } from './characters-transformation/characters-transformation.component';
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    CardsComponent
+    CardsComponent,
+    CharactersTransformationComponent
   ],
   imports: [
     CommonModule
